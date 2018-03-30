7315d9f13fdffa74639f174295298dd8474c534d
renanFaveri <renan_leal@estudante.sc.senai.br>
2018-03-30 00:55:30 +0000

    Sair, Entrar e Editar e tratativas de erro

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329004048) do
+ActiveRecord::Schema.define(version: 20180330004750) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(version: 20180329004048) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "phone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
